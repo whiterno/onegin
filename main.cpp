6696dc7bf603f7b9e060baff59bd6a584aaa5d9d
whiterno <timofeikryuchkov@gmail.com>
2024-09-13 02:04:38 +0300

    everything without void*

--- main.cpp
+++ main.cpp
@@ -4,39 +4,28 @@
 #include <assert.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "onegin.h"
 
 int main(void){
-    char * text;
+    char * text = NULL;
     FILE* fp = fopen(FILENAME_OUT, "w");
+    PtrTxt ptr_txt = {.ptr_array = NULL, .line_amount = 0};
 
-    PtrTxt left_to_right = {.line_amount = 0};
+    text = readFile(&ptr_txt);
 
-    text = readFile(&left_to_right);
+    sortPrint(ptr_txt, myLinecmp, fp);
+    sortPrint(ptr_txt, myLinecmpRev, fp);
+    fputs(text, fp);
 
-    bubbleSort(&left_to_right);
-
-    for (int i = 0; i < left_to_right.line_amount; i++){
-        if (*left_to_right.ptr_array[i] != '\n'){
-            fputsUntilNewline(left_to_right.ptr_array[i], fp);
-        }
-    }
-
-    fputc('\n', fp);
-    fputsUntilNewline(LINE, fp);
-    fputc('\n', fp);
+    fclose(fp);
+    freeAll(text, &ptr_txt);
 }
 
-int myStrcmp(const char* str1, const char* str2, const int* add_array, const int inx){
-    assert(str1);
-    assert(str2);
-
-    int num1 = add_array[inx], num2 = add_array[inx + 1];
-
-    str1 += num1;
-    str2 += num2;
-
-
+int myLinecmp(const Line line1, const Line line2){
+    char* str1 = line1.ptr_line + line1.skip_length;
+    char* str2 = line2.ptr_line + line2.skip_length;
     while (*str1 != '\n' && *str2 != '\n'){
         if (tolower(*str1 ) - tolower(*str2) != 0){
             return tolower(*str1) - tolower(*str2);
@@ -53,6 +42,31 @@ int myStrcmp(const char* str1, const char* str2, const int* add_array, const int
     return *str1;
 }
 
+int myLinecmpRev(const Line line1, const Line line2){
+    int length1 = line1.length - line1.skip_length_end + 1;
+    int length2 = line2.length - line2.skip_length_end + 1;
+
+    char* str1 = line1.ptr_line + length1 - 1;
+    char* str2 = line2.ptr_line + length2 - 1;
+
+    while(length1 > 0 && length2 > 0){
+        if (tolower(*str1) - tolower(*str2) != 0){
+            return tolower(*str1) - tolower(*str2);
+        }
+        str1--;
+        str2--;
+        length1--;
+        length2--;
+    }
+    if (length1 == 0 && length2 == 0){
+        return 0;
+    }
+    if (length1 == 0){
+        return -*str2;
+    }
+    return *str1;
+}
+
 size_t fileSize(const char* filename){
     assert(filename);
 
@@ -64,51 +78,37 @@ size_t fileSize(const char* filename){
 char* readFile(PtrTxt * ptr_txt){
     assert(ptr_txt);
 
-    char* text = (char*)calloc(fileSize(FILENAME) + 1, sizeof(char));
-    FILE* fp = fopen(FILENAME, "r");
-    int character = 0;
-    size_t amount = 0;
+    size_t fsize = fileSize(FILENAME);
+    char* text = (char*)calloc(fsize + 2, sizeof(char));
+    int fp = open(FILENAME, O_RDONLY);
 
-    while ((character = fgetc(fp))){
-        if (character == EOF){
-            break;
-        }
-        *(text + amount) = character;
-        amount++;
-        if (character == '\n'){
-            (ptr_txt->line_amount)++;
-        }
-    }
-    if (*(text + amount - 1) != '\n'){
-        (ptr_txt->line_amount)++;
-    }
-    *(text + amount) = '\0';
+    read(fp, text, fsize);
+    close(fp);
 
-    ptr_txt->ptr_array = makePtrArray(text, ptr_txt->line_amount);
+    text[fsize] = '\n';
+    text[fsize + 1] = '\0';
 
-    ptr_txt->skip_length = (int*)calloc(ptr_txt->line_amount, sizeof(int));
-    for (int i = 0; i < ptr_txt->line_amount; i++){
-        sscanf((ptr_txt->ptr_array)[i], "%*[!-@[-`{-~]%n", ptr_txt->skip_length + i);
-    }
+    ptr_txt->line_amount = countLines(text);
+    fillPtrTxt(ptr_txt, text);
 
     return text;
 }
 
-char ** makePtrArray(char* text, const int line_amount){
+Line* initializeLines(char* text, const int line_amount){
     assert(text);
 
-    size_t amount = 0;
-    int line = 1;
-    char ** ptr_arr = (char**)calloc(line_amount, sizeof(char*));
+    int line = 0;
+    Line* ptr_arr = (Line*)calloc(line_amount, sizeof(Line));
 
-    *ptr_arr = text;
-    while (*(text + amount) != '\0'){
-        if (*(text + amount) == '\n'){
-            *(ptr_arr + line) = text + amount + 1;
-            line++;
-        }
-        amount++;
+    while (*text != '\0'){
+        int length = (int)(strchr(text, '\n') - text);
+
+        (ptr_arr + line)->ptr_line = text;
+        (ptr_arr + line)->length = length;
+        text += length + 1;
+        line++;
     }
+
     return ptr_arr;
 }
 
@@ -128,25 +128,126 @@ void fputsUntilNewline(const char* ptr, FILE* fp){
     }while (*(ptr + amount++) != '\n');
 }
 
-void swapPointers(char ** ptr1, char** ptr2){
-    char* tmp = *ptr1;
-    *ptr1 = *ptr2;
-    *ptr2 = tmp;
+void swapLines(Line* ptr_array, int inx1, int inx2){
+    assert(ptr_array);
+
+    Line tmp_line = ptr_array[inx1];
+    ptr_array[inx1] = ptr_array[inx2];
+    ptr_array[inx2] = tmp_line;
 }
 
-void swapAdds(int* num1, int* num2){
-    int tmp = *num1;
-    *num1 = *num2;
-    *num2 = tmp;
-}
+void bubbleSort(Line* ptr_array, int line_amount, cmp_t compare){
+    assert(ptr_array);
 
-void bubbleSort(PtrTxt* ptr_txt){
-    for (int i = 0; i < ptr_txt->line_amount - 1; i++){
-        for (int j = 0; j < ptr_txt->line_amount - 1 - i; j++){
-            if (myStrcmp(ptr_txt->ptr_array[j], ptr_txt->ptr_array[j + 1], ptr_txt->skip_length, j) > 0){
-                swapAdds(ptr_txt->skip_length + j, ptr_txt->skip_length + j + 1);
-                swapPointers(ptr_txt->ptr_array + j, ptr_txt->ptr_array + j + 1);
+    for (int i = 0; i < line_amount - 1; i++){
+        for (int j = 0; j < line_amount - 1 - i; j++){
+            if (compare(ptr_array[j], ptr_array[j + 1]) > 0){
+                swapLines(ptr_array, j, j + 1);
             }
         }
     }
+}
+
+int skipTypoSymbols(char* string, int step, int length){
+    assert(string);
+
+    int amount = 0;
+
+    while (true){
+        if (length-- && ((*string < 'A') || (*string > 'Z' && *string < 'a') || (*string > 'z'))){
+            amount++;
+            string += step;
+            continue;
+        }
+        break;
+    }
+
+    return amount;
+}
+
+void fillPtrTxt(PtrTxt* ptr_txt, char* text){
+    assert(text);
+    assert(ptr_txt);
+
+    ptr_txt->ptr_array = initializeLines(text, ptr_txt->line_amount);
+
+    for (int i = 0; i < ptr_txt->line_amount; i++){
+        (ptr_txt->ptr_array + i)->skip_length = skipTypoSymbols(ptr_txt->ptr_array[i].ptr_line, 1, ptr_txt->ptr_array[i].length);
+    }
+    for (int i = 0; i < ptr_txt->line_amount; i++){
+        char* end_line = ptr_txt->ptr_array[i].ptr_line + ptr_txt->ptr_array[i].length;
+        (ptr_txt->ptr_array + i)->skip_length_end = skipTypoSymbols(end_line, -1, ptr_txt->ptr_array[i].length);
+    }
+}
+
+void quickSort(Line* ptr_array, int start, int end, cmp_t compare){
+    assert(ptr_array);
+
+    if (start < end){
+        int left = start, right = end;
+        int middle = (left + right) / 2;
+        Line middle_el = ptr_array[middle];
+
+        while(left <= right){
+            while (compare(ptr_array[left], middle_el) < 0){
+                left++;
+            }
+            while (compare(ptr_array[right], middle_el) > 0){
+                right--;
+            }
+            if (left <= right){
+                swapLines(ptr_array, left, right);
+                left++;
+                right--;
+            }
+        }
+        quickSort(ptr_array, start, right, compare);
+        quickSort(ptr_array, left, end, compare);
+    }
+}
+
+int countLines(char* text){
+    assert(text);
+
+    int line_amount = 0;
+    while (*text != '\0'){
+        text = strchr(text, '\n') + 1;
+        line_amount++;
+    }
+    return line_amount;
+}
+
+void fprintTxt(PtrTxt ptr_txt, FILE* fp){
+    assert(fp);
+
+    for (int i = 0; i < ptr_txt.line_amount; i++){
+        if (*ptr_txt.ptr_array[i].ptr_line != '\n'){
+            fputsUntilNewline(ptr_txt.ptr_array[i].ptr_line, fp);
+        }
+    }
+}
+
+void freeAll(char* text, PtrTxt* ptr_txt){
+    assert(text);
+    assert(ptr_txt);
+
+    for (int i = 0; i < ptr_txt->line_amount; i++){
+        ptr_txt->ptr_array[i].ptr_line = NULL;
+    }
+
+    free(ptr_txt->ptr_array);
+    free(text);
+}
+
+void fprintLine(FILE* fp){
+    fputc('\n', fp);
+    fputsUntilNewline(LINE, fp);
+    fputc('\n', fp);
+}
+
+void sortPrint(PtrTxt ptr_txt, cmp_t compare, FILE* fp){
+    quickSort(ptr_txt.ptr_array, 0, ptr_txt.line_amount - 1, compare);
+    fprintTxt(ptr_txt, fp);
+
+    fprintLine(fp);
 }
--- onegin.h
+++ onegin.h
@@ -1,25 +1,46 @@
 #ifndef ONEGIN_HEADER
 #define ONEGIN_HEADER
 
+enum StringCmp{
+    START,
+    END
+};
+
+struct Line{
+    char* ptr_line;
+    int skip_length;
+    int skip_length_end;
+    int length;
+};
+
 struct PtrTxt{
-    char ** ptr_array;
-    int * skip_length;
+    Line* ptr_array;
     int line_amount;
 };
+
+typedef int (*cmp_t)(const Line line1, const Line line2);
 
 const char FILENAME[] = "./onegin";
 const char FILENAME_OUT[] = "./onegin_sorted";
 const char LINE[] = "--------------------------------------------------------------\n";
 const int MAX_READ_LENGTH = 100;
 
-int myStrcmp(const char* str1, const char* str2, const int* add_array, const int inx);
+int myLinecmp(const Line line1, const Line line2);
+int myLinecmpRev(const Line line1, const Line line2);
 size_t fileSize(const char* filename);
 char* readFile(PtrTxt * ptr_txt);
-char ** makePtrArray(char* text, const int line_amount);
+Line* initializeLines(char* text, const int line_amount);
 void putsUntilNewline(const char* ptr);
 void fputsUntilNewline(const char* ptr, FILE* fp);
-void swapPointers(char ** ptr1, char** ptr2);
-void swapAdds(int* num1, int* num2);
-void bubbleSort(PtrTxt* ptr_txt);
+void swapLines(Line* ptr_array, int inx1, int inx2);
+void bubbleSort(Line* ptr_array, int line_amount, cmp_t compare);
+void quickSort(Line* ptr_array, int start, int end, cmp_t compare);
+int skipTypoSymbols(char* string, int step, int length);
+void fillPtrTxt(PtrTxt* ptr_txt, char* text);
+int countLines(char* text);
+void fprintTxt(PtrTxt ptr_txt, FILE* fp);
+void freeAll(char* text, PtrTxt* ptr_txt);
+void fprintLine(FILE* fp);
+void sortPrint(PtrTxt ptr_txt, cmp_t compare, FILE* fp);
 
 #endif
